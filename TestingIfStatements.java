4870d06f7ec0155c0e185aea410b7dcd858b163a
franyatta <95552724+franyatta@users.noreply.github.com>
2023-10-10 16:19:39 -0400

    Add files via upload

---
+++ TestingIfStatements.java
@@ -0,0 +1,15 @@
+public class TestingIfStatements {
+
+	public static void main(String[] args) {
+	
+	int i = 3, j = 2, k = 3;
+	
+	if (i > j) {
+		if (i > k)
+			System.out.println("A");
+	
+	else
+		System.out.println("B");
+	}
+	}
+}
